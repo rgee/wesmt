784c0260607f587d3c1966ddb150feb20c6896f7
rgee <rgee@wesleyan.edu>
2010-03-03 11:28:50 -0700

    Added a comment to better explain my mixed use of const.

--- src/vector2D.h
+++ src/vector2D.h
@@ -13,9 +13,13 @@ public:
 	
 	virtual ~Vector2D(){ };
 	
+        /* C++ "trickery" point:
+              Declaring const methods means a lot. I misspoke in the meeting.
+              It tells the compiler which functions you can call on a const object or not.
+         */
 	float GetMagnitude() const;
 	float x() const {return _x;}
-	float y() const{return _y;}
+	float y() const {return _y;}
 	
 	Vector2D operator*(float scalar) const { return Vector2D(this->_x * scalar, this->_y * scalar); }
 	Vector2D operator+(const Vector2D &vec) const { return Vector2D(this->_x + vec.x(), this->_y + vec.y()); }
